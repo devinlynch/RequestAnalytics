d71d5a69fa5b2065c60edd92dafaef0fd1ce44dd
devinlynch <devin.lynch.is@gmail.com>
2015-02-12 21:26:45 -0500

    Moved interceptor to framework

---
+++ RequestAnalytics/src/com/devinlynch/analytics/web/AnalyticsInterceptor.java
@@ -0,0 +1,39 @@
+package com.devinlynch.analytics.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.devinlynch.analytics.RequestAnalytics;
+import com.devinlynch.analytics.context.PersistedFileAnalyticsContext;
+
+public class AnalyticsInterceptor extends HandlerInterceptorAdapter {
+	private RequestAnalytics analytics;
+	
+	public AnalyticsInterceptor() {
+		analytics = new RequestAnalytics(new PersistedFileAnalyticsContext("requestAnalytsics", 5000));
+	}
+	
+	
+	@Override
+	public boolean preHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler) throws Exception {
+		analytics.logRequestStarted(request);
+			
+		return true;
+    }
+	
+	@Override
+	public void postHandle(
+			HttpServletRequest request, 
+			HttpServletResponse response, 
+			Object handler, 
+			ModelAndView modelAndView) {
+		
+		analytics.logRequestFinished(request);
+	}
+}
